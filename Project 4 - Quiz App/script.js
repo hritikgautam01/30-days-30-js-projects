f2028bcd7b529548a0cbf310ff7093aeb01726dd
Hritik Gautam <hritikgautam19@gmai.com>
2026-02-16 14:31:10 +0530

    Quiz App project completion

---
+++ Project 4 - Quiz App/script.js
@@ -0,0 +1,122 @@
+const questions = [
+    {
+        question : "Which is largest animal in the world",
+        answers : [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question : "Which is the smallest continent in the world",
+        answers : [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+    {
+        question : "Which is largest desert in the world",
+        answers : [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true}
+        ]
+    }
+]
+
+const questionElement = document.getElementById("question")
+const answerBtns = document.getElementById("answer-btn")
+const nextBtn = document.getElementById("next-btn")
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextBtn.innerHTML = "Next"
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    
+    // Code for displaying the answers
+    currentQuestion.answers.forEach(answer =>{
+        const button = document.createElement("button")
+        button.innerHTML = answer.text;
+        button.classList.add("btn")
+        answerBtns.appendChild(button)
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer)
+    })
+}
+
+function resetState(){
+    nextBtn.style.display = "none"
+    while(answerBtns.firstChild){
+        answerBtns.removeChild(answerBtns.firstChild);
+    }
+}
+
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerBtns.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    })
+    nextBtn.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}`
+    nextBtn.innerHTML = "Play Again"
+    nextBtn.style.display = "block";
+}
+
+
+function handleNextbtn(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+nextBtn.addEventListener('click', () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextbtn();
+    }
+    else{
+        startQuiz();
+    }
+})
+
+
+startQuiz();
